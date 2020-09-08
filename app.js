1b45b3198a3fa13c1a462775934f52ecac2645fa
Danny <tekhexrax@yahoo.com>
2020-09-07 21:24:59 -0400

    Merged js back into one file

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-
-const method = require("./assets/js/methods.js") 
-
 const userChoice = [
   "View All Departments",
   "View All Roles",
@@ -42,31 +39,31 @@ promptUser = () => {
     .then((answer) => {
       switch (answer.choice) {
         case userChoice[0]:
-          method.viewDept();
+          viewDept();
           break;
 
         case userChoice[1]:
-          method.viewRole();
+          viewRole();
           break;
 
         case userChoice[2]:
-          method.viewEmployee();
+          viewEmployee();
           break;
 
         case userChoice[3]:
-          method.addDept();
+          addDept();
           break;
 
         case userChoice[4]:
-          method.addRole();
+          addRole();
           break;
 
         case userChoice[5]:
-          method.addEmployee();
+          addEmployee();
           break;
 
         case userChoice[6]:
-          method.updateEmployee();
+          updateEmployee();
           break;
 
         case userChoice[7]:
@@ -76,6 +73,82 @@ promptUser = () => {
     });
 };
 
-module.exports = {
-  promptUser
-}
+// View departments
+
+viewDept = () => {
+  connection.query("SELECT * FROM employee_tracker.department;", function (
+    err,
+    results
+  ) {
+    for (i = 0; i < results.length; i++) {
+      console.table([
+        {
+          Id: results[i].id,
+          Department: results[i].name,
+        },
+      ]);
+    }
+    promptUser();
+    if (err) throw err;
+  });
+};
+
+// View Roles
+
+viewRole = () => {
+  connection.query("SELECT * FROM employee_tracker.role;", function (
+    err,
+    results
+  ) {
+    for (i = 0; i < results.length; i++) {
+      console.table([
+        {
+          Id: results[i].id,
+          Title: results[i].title,
+          Salary: results[i].salary,
+          Department_Id: results[i].department_id,
+        },
+      ]);
+    }
+    promptUser();
+    if (err) throw err;
+  });
+};
+
+// View Employees
+
+viewEmployee = () => {
+  connection.query("SELECT * FROM employee_tracker.employee;", function (
+    err,
+    results
+  ) {
+    for (i = 0; i < results.length; i++) {
+      console.table([
+        {
+          Id: results[i].id,
+          First_Name: results[i].first_name,
+          Last_Name: results[i].last_name,
+          Role_Id: results[i].role_id,
+        },
+      ]);
+    }
+    promptUser();
+    if (err) throw err;
+  });
+};
+
+// Add Departments
+
+addDept = () => {};
+
+// Add Roles
+
+addRole = () => {};
+
+// Add Employees
+
+addEmployee = () => {};
+
+// Update Employee
+
+updateEmployee = () => {};
